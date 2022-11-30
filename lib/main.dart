54b7b17264ff09eb08f5a71cd325b55e0328cbed
kanaydo <batara.girsang@outlook.com>
2022-11-30 12:33:10 +0700

    fisishin list

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:composing/chat_item.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,13 +14,28 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyPage(),
+      home: MyPage(),
     );
   }
 }
 
 class MyPage extends StatelessWidget {
-  const MyPage({Key? key}) : super(key: key);
+  MyPage({Key? key}) : super(key: key);
+
+  final data = [
+    "januari",
+    "februari",
+    "maret",
+    "april",
+    "mei",
+    "juni",
+    "juli",
+    "agustus",
+    "september",
+    "oktober",
+    "novermber",
+    "desember"
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -27,50 +43,11 @@ class MyPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('latihan composing'),
       ),
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            decoration: BoxDecoration(
-              color: Colors.lightBlue.shade100,
-              border: Border.all(
-                color: Colors.red,
-                width: 2
-              ),
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20)
-              )
-            ),
-            child: Row(
-              children: [
-                const CircleAvatar(
-                  backgroundColor: Colors.redAccent,
-                  child: FlutterLogo(),
-                ),
-                Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const Text('Bayu Angga Wijaya'),
-                          Row(
-                            children: const [
-                              Icon(Icons.check),
-                              Text('okee')
-                            ],
-                          )
-                        ],
-                      ),
-                    )
-                ),
-                const Text('yesterday')
-              ],
-            ),
-          ),
-        ],
+      body: ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (context, index) {
+          return ChatItem();
+        },
       ),
     );
   }
