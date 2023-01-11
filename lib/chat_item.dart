49bafe6a194a408889b884e7a3ddc6951e7ebeb3
kanaydo <batara.girsang@outlook.com>
2023-01-11 12:34:56 +0700

    add chat page

--- lib/chat_item.dart
+++ lib/chat_item.dart
@@ -1,3 +1,4 @@
+import 'package:composing/chat_page.dart';
 import 'package:flutter/material.dart';
 
 import 'main.dart';
@@ -9,63 +10,68 @@ class ChatItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(8),
-      padding: EdgeInsets.symmetric(horizontal: 8),
-      decoration: BoxDecoration(
-          color: Colors.lightBlue.shade100,
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20),
-              bottomRight: Radius.circular(20)
-          ),
-          border: Border.all(
-              color: Colors.red,
-              width: 2
-          )
-      ),
-      child: Row(
-        children: [
-          const CircleAvatar(
-            backgroundColor: Colors.redAccent,
-            child: FlutterLogo(),
-          ),
-          Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      chatData.name,
-                      style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    Row(
-                      children:  [
-                        const Icon(
-                          Icons.check,
-                          size: 15,
-                          color: Colors.lightBlue,
-                        ),
-                        Text(chatData.lastChat)
-                      ],
-                    )
-                  ],
-                ),
-              )
-          ),
-          Text(
-            chatData.lastSeen,
-            style: TextStyle(
-                fontSize: 10,
-                fontStyle: FontStyle.italic,
-                color: Colors.grey
+    return InkWell(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => ChatPage(chatData: chatData)));
+      },
+      child: Container(
+        margin: EdgeInsets.all(8),
+        padding: EdgeInsets.symmetric(horizontal: 8),
+        decoration: BoxDecoration(
+            color: Colors.lightBlue.shade100,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20)
             ),
-          )
-        ],
+            border: Border.all(
+                color: Colors.red,
+                width: 2
+            )
+        ),
+        child: Row(
+          children: [
+            const CircleAvatar(
+              backgroundColor: Colors.redAccent,
+              child: FlutterLogo(),
+            ),
+            Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        chatData.name,
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      Row(
+                        children:  [
+                          const Icon(
+                            Icons.check,
+                            size: 15,
+                            color: Colors.lightBlue,
+                          ),
+                          Text(chatData.lastChat)
+                        ],
+                      )
+                    ],
+                  ),
+                )
+            ),
+            Text(
+              chatData.lastSeen,
+              style: TextStyle(
+                  fontSize: 10,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.grey
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
